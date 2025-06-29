0ab2b7c81cbd0b8833ef1a088729badd7ff8c68b
likanug <likanug.g@qq.com>
2025-06-29 21:35:51 +0800

    feat: implement custom title bar with window controls and drag functionality

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 // #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
 use eframe::egui;
+use eframe::egui::{UiBuilder, ViewportCommand};
 use egui::{FontDefinitions, FontFamily};
 use std::sync::Arc;
 
@@ -9,10 +10,10 @@ fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([640.0, 480.0]) // wide enough for the drag-drop overlay text
-            .with_drag_and_drop(true)
-            .with_resizable(true),
-        // Enable drag-and-drop support for the viewport.
+            .with_decorations(false)  // Hide the OS-specific "chrome" around the window
+            .with_inner_size([640.0, 480.0])// Initial size of the window
+            .with_drag_and_drop(true)  // wide enough for the drag-drop overlay text
+            .with_resizable(true),  // Allow resizing the window
         ..Default::default()
     };
     eframe::run_native(
@@ -34,13 +35,37 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Set a custom Chinese font for the application.
         set_chinese_font(ctx);
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Drag-and-drop files onto the window!");
+        let _panel_frame = egui::Frame::new()
+            .fill(ctx.style().visuals.window_fill())
+            .corner_radius(10)
+            .stroke(ctx.style().visuals.widgets.noninteractive.fg_stroke)
+            .outer_margin(1); // so the stroke is within the bounds
+
+        egui::CentralPanel::default().frame(_panel_frame).show(ctx, |ui| {
+            let app_rect = ui.max_rect();
+
+            let title_bar_height = 32.0;
+            let title_bar_rect = {
+                let mut rect = app_rect;
+                rect.max.y = rect.min.y + title_bar_height;
+                rect
+            };
+            title_bar_ui(ui, title_bar_rect, "Editor-rs");
+
+            // Add the contents:
+            let _content_rect = {
+                let mut rect = app_rect;
+                rect.min.y = title_bar_rect.max.y;
+                rect
+            }.shrink(4.0);
+            let _content_ui = ui.new_child(UiBuilder::new().max_rect(_content_rect));
             // Show the theme preference buttons:
             ui.horizontal(|ui| {
-                ui.label("egui theme:");
+                ui.label("theme:");
                 egui::widgets::global_theme_preference_buttons(ui);
             });
+            ui.label("Drag-and-drop files onto the window!");
+
             if ui.button("Open file…").clicked() {
                 if let Some(path) = rfd::FileDialog::new().pick_file() {
                     self.picked_path = Some(path.display().to_string());
@@ -121,6 +146,10 @@ impl eframe::App for MyApp {
             }
         });*/
     }
+
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        egui::Rgba::TRANSPARENT.to_array() // Make sure we don't paint anything behind the rounded corners
+    }
 }
 
 /** * Set a custom Chinese font for the application.
@@ -148,6 +177,98 @@ fn set_chinese_font(ctx: &egui::Context) {
         .insert(0, "my_chinese".to_owned());
 
     ctx.set_fonts(fonts);
+}
+
+fn title_bar_ui(ui: &mut egui::Ui, title_bar_rect: eframe::epaint::Rect, title: &str) {
+    use egui::{vec2, Align2, FontId, Id, PointerButton, Sense, UiBuilder};
+
+    let painter = ui.painter();
+
+    let title_bar_response = ui.interact(
+        title_bar_rect,
+        Id::new("title_bar"),
+        Sense::click_and_drag(),
+    );
+
+    // Paint the title:
+    painter.text(
+        title_bar_rect.center(),
+        Align2::CENTER_CENTER,
+        title,
+        FontId::proportional(20.0),
+        ui.style().visuals.text_color(),
+    );
+
+    // Paint the line under the title:
+    painter.line_segment(
+        [
+            title_bar_rect.left_bottom() + vec2(1.0, 0.0),
+            title_bar_rect.right_bottom() + vec2(-1.0, 0.0),
+        ],
+        ui.visuals().widgets.noninteractive.bg_stroke,
+    );
+
+    // Interact with the title bar (drag to move window):
+    if title_bar_response.double_clicked() {
+        let is_maximized = ui.input(|i| i.viewport().maximized.unwrap_or(false));
+        ui.ctx()
+            .send_viewport_cmd(ViewportCommand::Maximized(!is_maximized));
+    }
+
+    if title_bar_response.drag_started_by(PointerButton::Primary) {
+        ui.ctx().send_viewport_cmd(ViewportCommand::StartDrag);
+    }
+
+    ui.scope_builder(
+        UiBuilder::new()
+            .max_rect(title_bar_rect)
+            .layout(egui::Layout::right_to_left(egui::Align::Center)),
+        |ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.visuals_mut().button_frame = false;
+            ui.add_space(8.0);
+            close_maximize_minimize(ui);
+        },
+    );
+}
+
+/// Show some close/maximize/minimize buttons for the native window.
+fn close_maximize_minimize(ui: &mut egui::Ui) {
+    use egui::{Button, RichText};
+
+    let button_height = 12.0;
+
+    let close_response = ui
+        .add(Button::new(RichText::new("❌").size(button_height)))
+        .on_hover_text("Close the window");
+    if close_response.clicked() {
+        ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
+    }
+
+    let is_maximized = ui.input(|i| i.viewport().maximized.unwrap_or(false));
+    if is_maximized {
+        let maximized_response = ui
+            .add(Button::new(RichText::new("🗗").size(button_height)))
+            .on_hover_text("Restore window");
+        if maximized_response.clicked() {
+            ui.ctx()
+                .send_viewport_cmd(ViewportCommand::Maximized(false));
+        }
+    } else {
+        let maximized_response = ui
+            .add(Button::new(RichText::new("🗗").size(button_height)))
+            .on_hover_text("Maximize window");
+        if maximized_response.clicked() {
+            ui.ctx().send_viewport_cmd(ViewportCommand::Maximized(true));
+        }
+    }
+
+    let minimized_response = ui
+        .add(Button::new(RichText::new("🗕").size(button_height)))
+        .on_hover_text("Minimize the window");
+    if minimized_response.clicked() {
+        ui.ctx().send_viewport_cmd(ViewportCommand::Minimized(true));
+    }
 }
 /*
 /// Preview hovering files:
